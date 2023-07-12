daff6da9887266b657efb0a998f9c703693e7ed0
arroyojavier.sanandres@gmail.com <arroyojavier.sanandres@gmail.com>
2023-07-12 15:41:24 -0400

    esquema prima actualizado agregado scripts sql al proyecto

---
+++ prisma/migrations/20230712184539_complete_model/migration.sql
@@ -0,0 +1,368 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "active_visa" (
+    "id" BIGSERIAL NOT NULL,
+    "country" VARCHAR(18) NOT NULL,
+
+    CONSTRAINT "active_visa_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "availability" (
+    "id" BIGSERIAL NOT NULL,
+    "availability" VARCHAR(10) NOT NULL,
+
+    CONSTRAINT "availability_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "better_current_situation" (
+    "id" BIGSERIAL NOT NULL,
+    "situation" VARCHAR NOT NULL,
+
+    CONSTRAINT "better_current_situation_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies" (
+    "id" BIGSERIAL NOT NULL,
+    "first_name" VARCHAR(20) NOT NULL,
+    "last_name" VARCHAR(20) NOT NULL,
+    "email" VARCHAR(30) NOT NULL,
+    "phone_number" VARCHAR(12) NOT NULL,
+    "company" VARCHAR(40) NOT NULL,
+    "questions" TEXT,
+
+    CONSTRAINT "companies_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "companies_to_position" (
+    "id" BIGSERIAL NOT NULL,
+    "id_companies" BIGINT NOT NULL,
+    "id_target_position" BIGINT NOT NULL,
+
+    CONSTRAINT "companies_to_position_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "current_job_status" (
+    "id" BIGSERIAL NOT NULL,
+    "state" VARCHAR NOT NULL,
+
+    CONSTRAINT "current_job_status_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "education_received" (
+    "id" BIGSERIAL NOT NULL,
+    "career" VARCHAR(80) NOT NULL,
+    "name_institution" VARCHAR(60) NOT NULL,
+    "type_institution" VARCHAR(60) NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "education_received_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "frameworks_or_batabase" (
+    "id" BIGSERIAL NOT NULL,
+    "technology_name" VARCHAR(28) NOT NULL,
+
+    CONSTRAINT "frameworks_or_batabase_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile_active_visa" (
+    "id" BIGSERIAL NOT NULL,
+    "id_active_visa" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "profile_active_visa_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile_current_situation" (
+    "id" BIGSERIAL NOT NULL,
+    "id_better_current_situation" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "profile_current_situation_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile_to_availability" (
+    "id" BIGSERIAL NOT NULL,
+    "id_availability" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "profile_to_availability_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile_to_soft_skill" (
+    "id" BIGSERIAL NOT NULL,
+    "id_soft_skills" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "profile_to_soft_skill_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "profile_to_years_exp" (
+    "id" BIGSERIAL NOT NULL,
+    "id_years_experience" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "profile_to_years_exp_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "programming_language" (
+    "id" BIGSERIAL NOT NULL,
+    "language" VARCHAR(28) NOT NULL,
+
+    CONSTRAINT "programming_language_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "soft_skills" (
+    "id" BIGSERIAL NOT NULL,
+    "skill" VARCHAR(28) NOT NULL,
+
+    CONSTRAINT "soft_skills_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "target_position" (
+    "id" BIGSERIAL NOT NULL,
+    "position" VARCHAR(60) NOT NULL,
+
+    CONSTRAINT "target_position_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "technical_test_tools" (
+    "id" BIGSERIAL NOT NULL,
+    "approved" BOOLEAN,
+    "score" INTEGER,
+    "date_completed" DATE,
+    "id_tools" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "technical_test_tools_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "technology_expertise" (
+    "id" BIGSERIAL NOT NULL,
+    "level" INTEGER NOT NULL,
+    "others" TEXT,
+    "id_programming_language" BIGINT,
+    "id_frameworks_or_batabase" BIGINT,
+    "id_tools" BIGINT,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "technology_expertise_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tools" (
+    "id" BIGSERIAL NOT NULL,
+    "tool" VARCHAR(28) NOT NULL,
+
+    CONSTRAINT "tools_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tt_framework_base" (
+    "id" BIGSERIAL NOT NULL,
+    "approved" BOOLEAN,
+    "score" INTEGER,
+    "date_completed" DATE,
+    "id_frameworks_or_batabase" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "tt_framework_base_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tt_pro_language" (
+    "id" BIGSERIAL NOT NULL,
+    "approved" BOOLEAN,
+    "score" INTEGER,
+    "date_completed" DATE,
+    "id_programming_language" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "technical_test_pro_language_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" BIGSERIAL NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "email" VARCHAR(30) NOT NULL,
+    "password" VARCHAR,
+    "id_user_role" BIGINT NOT NULL,
+
+    CONSTRAINT "user_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_profile" (
+    "id" BIGSERIAL NOT NULL,
+    "phone_number" VARCHAR(12) NOT NULL,
+    "city" VARCHAR(20) NOT NULL,
+    "country" VARCHAR(18) NOT NULL,
+    "genre" VARCHAR(20) NOT NULL,
+    "highest_edu_level" VARCHAR(20) NOT NULL,
+    "current_edu_status" VARCHAR(20) NOT NULL,
+    "english_level" VARCHAR(16) NOT NULL,
+    "url_cv" VARCHAR,
+    "url_linkedin" VARCHAR,
+    "url_github" VARCHAR,
+    "url_portfolio" VARCHAR,
+    "preferred_project" TEXT,
+    "work_expectation" TEXT,
+    "id_current_job_status" BIGINT NOT NULL,
+    "id_user" BIGINT NOT NULL,
+
+    CONSTRAINT "user_profile_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_role" (
+    "id" BIGSERIAL NOT NULL,
+    "role" VARCHAR NOT NULL,
+
+    CONSTRAINT "user_role_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_to_position" (
+    "id" BIGSERIAL NOT NULL,
+    "id_target_position" BIGINT NOT NULL,
+    "id_user_profile" BIGINT NOT NULL,
+
+    CONSTRAINT "user_to_position_pk" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "years_experience" (
+    "id" BIGSERIAL NOT NULL,
+    "years" VARCHAR(40) NOT NULL,
+
+    CONSTRAINT "years_experience_pk" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_current_situation_uq" ON "profile_current_situation"("id_user_profile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profile_to_years_exp_uq" ON "profile_to_years_exp"("id_user_profile");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "technical_test_tools_uq" ON "technical_test_tools"("id_tools");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tt_framework_base_uq" ON "tt_framework_base"("id_frameworks_or_batabase");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tt_pro_language_uq" ON "tt_pro_language"("id_programming_language");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_profile_uq" ON "user_profile"("id_user");
+
+-- AddForeignKey
+ALTER TABLE "companies_to_position" ADD CONSTRAINT "companies_fk" FOREIGN KEY ("id_companies") REFERENCES "companies"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "companies_to_position" ADD CONSTRAINT "target_position_fk" FOREIGN KEY ("id_target_position") REFERENCES "target_position"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "education_received" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_active_visa" ADD CONSTRAINT "active_visa_fk" FOREIGN KEY ("id_active_visa") REFERENCES "active_visa"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_active_visa" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_current_situation" ADD CONSTRAINT "better_current_situation_fk" FOREIGN KEY ("id_better_current_situation") REFERENCES "better_current_situation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_current_situation" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_availability" ADD CONSTRAINT "availability_fk" FOREIGN KEY ("id_availability") REFERENCES "availability"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_availability" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_soft_skill" ADD CONSTRAINT "soft_skills_fk" FOREIGN KEY ("id_soft_skills") REFERENCES "soft_skills"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_soft_skill" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_years_exp" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "profile_to_years_exp" ADD CONSTRAINT "years_experience_fk" FOREIGN KEY ("id_years_experience") REFERENCES "years_experience"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technical_test_tools" ADD CONSTRAINT "tools_fk" FOREIGN KEY ("id_tools") REFERENCES "tools"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technical_test_tools" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technology_expertise" ADD CONSTRAINT "frameworks_or_batabase_fk" FOREIGN KEY ("id_frameworks_or_batabase") REFERENCES "frameworks_or_batabase"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technology_expertise" ADD CONSTRAINT "programming_language_fk" FOREIGN KEY ("id_programming_language") REFERENCES "programming_language"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technology_expertise" ADD CONSTRAINT "tools_fk" FOREIGN KEY ("id_tools") REFERENCES "tools"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "technology_expertise" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tt_framework_base" ADD CONSTRAINT "frameworks_or_batabase_fk" FOREIGN KEY ("id_frameworks_or_batabase") REFERENCES "frameworks_or_batabase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tt_framework_base" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tt_pro_language" ADD CONSTRAINT "programming_language_fk" FOREIGN KEY ("id_programming_language") REFERENCES "programming_language"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tt_pro_language" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_role_fk" FOREIGN KEY ("id_user_role") REFERENCES "user_role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_profile" ADD CONSTRAINT "current_job_status_fk" FOREIGN KEY ("id_current_job_status") REFERENCES "current_job_status"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_profile" ADD CONSTRAINT "user_fk" FOREIGN KEY ("id_user") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_to_position" ADD CONSTRAINT "target_position_fk" FOREIGN KEY ("id_target_position") REFERENCES "target_position"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_to_position" ADD CONSTRAINT "user_profile_fk" FOREIGN KEY ("id_user_profile") REFERENCES "user_profile"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
