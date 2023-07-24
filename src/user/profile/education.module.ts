eba4f967b0ff43a2a02403e50c0dca9db699b2bc
arroyojavier.sanandres@gmail.com <arroyojavier.sanandres@gmail.com>
2023-07-23 20:57:44 -0400

    incidencia c10-37 middleware para decodificar token: refactorizacion de funcion para obtener id mediante token creada en incidencia c10-37 y creacion de middleware para decodificar token
    [Archivo education.module]
    importacion de middeware y configuracion para su uso en una ruta
    
    [Archivo user.middleware]
    definicion de middleware en el cual se captura el token y lo decodifica mediante el metodo getUserByToken definido en la misma clase del middleware para luego agregarlo a la request como la propiedad user

--- src/user/profile/education.module.ts
+++ src/user/profile/education.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { EducationController } from './education.controller';
 import { EducationService } from './education.service';
 import { AuthService } from 'src/Auth/auth.service';
 import { JwtStrategy } from 'src/Auth/strategy';
 import { JwtModule } from '@nestjs/jwt';
+import { DecodedTokenMiddleware } from 'src/middleware/user.middleware';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [EducationController],
-  providers: [EducationService, JwtStrategy, AuthService]
+  providers: [EducationService, AuthService]
 })
-export class EducationModule {}
+export class EducationModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+    .apply(DecodedTokenMiddleware)
+    .forRoutes('/profile/education/addEducation');
+  }
+  
+}
