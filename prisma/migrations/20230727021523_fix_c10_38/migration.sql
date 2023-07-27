9f0ac1dce14f471ca4b718266db40b75a5ee62f9
arroyojavier.sanandres@gmail.com <arroyojavier.sanandres@gmail.com>
2023-07-26 22:18:19 -0400

    arreglo de campos en modelo de prisma - cambios de not null

--- prisma/migrations/20230727021523_fix_c10_38/migration.sql
+++ prisma/migrations/20230727021523_fix_c10_38/migration.sql
@@ -18,16 +18,19 @@ ALTER TABLE "profile_to_years_exp" DROP CONSTRAINT "user_profile_fk";
 ALTER TABLE "profile_to_years_exp" DROP CONSTRAINT "years_experience_fk";
 
 -- DropForeignKey
-ALTER TABLE "user_profile" DROP CONSTRAINT "current_job_status_fk";
+ALTER TABLE "user" DROP CONSTRAINT "user_role_fk";
+
+-- AlterTable
+ALTER TABLE "user" ALTER COLUMN "first_name" DROP NOT NULL,
+ALTER COLUMN "last_name" DROP NOT NULL,
+ALTER COLUMN "id_user_role" DROP NOT NULL;
 
 -- AlterTable
 ALTER TABLE "user_profile" ADD COLUMN     "id_better_current_situation" INTEGER,
-ADD COLUMN     "id_current_job_status1" INTEGER,
 ADD COLUMN     "id_years_experience" INTEGER,
 ALTER COLUMN "highest_edu_level" DROP NOT NULL,
 ALTER COLUMN "current_edu_status" DROP NOT NULL,
-ALTER COLUMN "english_level" DROP NOT NULL,
-ALTER COLUMN "id_current_job_status" DROP NOT NULL;
+ALTER COLUMN "english_level" DROP NOT NULL;
 
 -- DropTable
 DROP TABLE "profile_current_situation";
@@ -36,10 +39,10 @@ DROP TABLE "profile_current_situation";
 DROP TABLE "profile_to_years_exp";
 
 -- AddForeignKey
-ALTER TABLE "user_profile" ADD CONSTRAINT "better_current_situation_fk" FOREIGN KEY ("id_better_current_situation") REFERENCES "better_current_situation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "user" ADD CONSTRAINT "user_role_fk" FOREIGN KEY ("id_user_role") REFERENCES "user_role"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "user_profile" ADD CONSTRAINT "current_job_status_fk" FOREIGN KEY ("id_current_job_status1") REFERENCES "current_job_status"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "user_profile" ADD CONSTRAINT "better_current_situation_fk" FOREIGN KEY ("id_better_current_situation") REFERENCES "better_current_situation"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "user_profile" ADD CONSTRAINT "years_experience_fk" FOREIGN KEY ("id_years_experience") REFERENCES "years_experience"("id") ON DELETE SET NULL ON UPDATE CASCADE;
