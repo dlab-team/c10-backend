8cadf4bde25a5e107cfc7a8eb64c93711aa3ed08
Oliver_rodriguez29 <mawiis455@gmail.com>
2023-07-28 14:21:48 -0500

    probando

--- src/job-profile/job-profile.service.ts
+++ src/job-profile/job-profile.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ForbiddenException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JobProfileDto } from './dto';
 
 @Injectable()
 export class JobProfileService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-    async getJobProfile() {
-        const users = await this.prisma.technology_expertise.findMany();
-        return users.map(({ ...rest }) => rest);
+  async getJobProfile() {
+    const users = await this.prisma.technology_expertise.findMany();
+    return users.map(({ ...rest }) => rest);
+  }
+
+  async createJobProfile(dto: JobProfileDto) {
+    try {
+      const technologyExpertise = await this.prisma.technology_expertise.create(
+        {
+          data: {
+            level: dto.level,
+            others: dto.others,
+            id_programming_language: 1,
+            id_frameworks_or_batabase: 1,
+            id_tools: 1,
+            id_user_profile: 1,
+          },
+        },
+      );
+      return technologyExpertise;
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ForbiddenException('This language already exists');
       }
-
-    async createJobProfile(dto: JobProfileDto){
-      const technologyExpertise = await this.prisma.technology_expertise.create({
-        data: {
-          level: dto.level,
-          others: dto.others,
-          id_programming_language: 1,
-          id_frameworks_or_batabase: 1,
-          id_tools: 1,
-          id_user_profile: 1,
-        }
-      })}
-      
+      throw error;
+    }
+  }
 }
