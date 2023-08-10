c647a1abc1f74467250369b4cffa10b713c56924
Elizabeth4812 <elizabethjerezr@gmail.com>
2023-08-09 23:58:13 -0400

    ok

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CompaniesService } from './companies.service';
+import { Controller, Post, Body } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
 
 @Controller('companies')
 export class CompaniesController {
-  constructor(private readonly companiesService: CompaniesService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
-    return this.companiesService.create(createCompanyDto);
-  }
+  async createCompany(@Body() createCompanyDto: CreateCompanyDto) {
+    const companyInput = {
+      first_name: createCompanyDto.first_name,
+      last_name: createCompanyDto.last_name,
+      email: createCompanyDto.email,
+      phone_number: createCompanyDto.phone_number,
+      company: createCompanyDto.company_name,
+      questions: createCompanyDto.questions,
+    };
 
-  @Get()
-  findAll() {
-    return this.companiesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companiesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
-    return this.companiesService.update(+id, updateCompanyDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesService.remove(+id);
+    return this.prisma.companies.create({
+      data: companyInput,
+    });
   }
 }
--- src/companies_to_position/companies_to_position.controller.ts
+++ src/companies_to_position/companies_to_position.controller.ts
@@ -1,34 +1,27 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CompaniesToPositionService } from './companies_to_position.service';
+import { Controller, Post, Body, Put, Param } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateCompaniesToPositionDto } from './dto/create-companies_to_position.dto';
 import { UpdateCompaniesToPositionDto } from './dto/update-companies_to_position.dto';
 
 @Controller('companies-to-position')
 export class CompaniesToPositionController {
-  constructor(private readonly companiesToPositionService: CompaniesToPositionService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Post()
-  create(@Body() createCompaniesToPositionDto: CreateCompaniesToPositionDto) {
-    return this.companiesToPositionService.create(createCompaniesToPositionDto);
+  async create(@Body() createDto: CreateCompaniesToPositionDto) {
+    return this.prisma.companies_to_position.create({
+      data: createDto,
+    });
   }
 
-  @Get()
-  findAll() {
-    return this.companiesToPositionService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.companiesToPositionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCompaniesToPositionDto: UpdateCompaniesToPositionDto) {
-    return this.companiesToPositionService.update(+id, updateCompaniesToPositionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.companiesToPositionService.remove(+id);
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateDto: UpdateCompaniesToPositionDto,
+  ) {
+    return this.prisma.companies_to_position.update({
+      where: { id },
+      data: updateDto,
+    });
   }
 }
