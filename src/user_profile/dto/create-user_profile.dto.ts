8a8d316afa38dc7019635c1240309e92fa6c2b26
Saitraru <leftaro.pumanqui@gmail.com>
2023-08-10 23:00:07 -0400

    Creación CRUD user profile

--- src/user_profile/dto/create-user_profile.dto.ts
+++ src/user_profile/dto/create-user_profile.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserProfileDto {}
+export class CreateUserProfileDto {
+  phone_number: string;
+  city: string;
+  country: string;
+  genre: string;
+  highest_edu_level: string | null;
+  current_edu_status: string | null;
+  english_level: string | null;
+  url_cv: string | null;
+  url_linkedin: string | null;
+  url_github: string | null;
+  url_portfolio: string | null;
+  preferred_project: string | null;
+  work_expectation: string | null;
+  id_current_job_status: number;
+  id_user: number;
+  id_years_experience: number | null;
+  id_better_current_situation: number | null;
+}
--- src/user_profile/user_profile.service.ts
+++ src/user_profile/user_profile.service.ts
@@ -1,26 +1,59 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserProfileDto } from './dto/create-user_profile.dto';
 import { UpdateUserProfileDto } from './dto/update-user_profile.dto';
 
 @Injectable()
 export class UserProfileService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createUserProfileDto: CreateUserProfileDto) {
-    return 'This action adds a new userProfile';
+    const userProfileInput = {
+      phone_number: createUserProfileDto.phone_number,
+      city: createUserProfileDto.city,
+      country: createUserProfileDto.country,
+      genre: createUserProfileDto.genre,
+      highest_edu_level: createUserProfileDto.highest_edu_level,
+      current_edu_status: createUserProfileDto.current_edu_status,
+      english_level: createUserProfileDto.english_level,
+      url_cv: createUserProfileDto.url_cv,
+      url_linkedin: createUserProfileDto.url_linkedin,
+      url_github: createUserProfileDto.url_github,
+      url_portfolio: createUserProfileDto.url_portfolio,
+      preferred_project: createUserProfileDto.preferred_project,
+      work_expectation: createUserProfileDto.work_expectation,
+      id_current_job_status: createUserProfileDto.id_current_job_status,
+      id_user: createUserProfileDto.id_user,
+      id_years_experience: createUserProfileDto.id_years_experience,
+      id_better_current_situation:
+        createUserProfileDto.id_better_current_situation,
+    };
+
+    return this.prisma.user_profile.create({
+      data: userProfileInput,
+    });
   }
 
   findAll() {
-    return `This action returns all userProfile`;
+    return this.prisma.user_profile.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} userProfile`;
+    return this.prisma.user_profile.findFirst({
+      where: { id: +id },
+    });
   }
 
   update(id: number, updateUserProfileDto: UpdateUserProfileDto) {
-    return `This action updates a #${id} userProfile`;
+    return this.prisma.user_profile.update({
+      where: { id: +id },
+      data: updateUserProfileDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} userProfile`;
+    return this.prisma.companies.delete({
+      where: { id: +id },
+    });
   }
 }
