191478506b4c069d7dc750d5bd063891aaea77af
Javier Arroyo <arroyojavier.sanandres@gmail.com>
2023-09-13 21:24:17 -0300

    agregado los endpoints para buscar los datos del usuarios, mediante token (uso de usuario) y por id para uso de administracion para los modulos education y job-sought

--- src/user/profile/education.controller.ts
+++ src/user/profile/education.controller.ts
@@ -1,14 +1,33 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
-import { EducationService } from './education.service';
-import { JwtGuard } from '../../Auth/guard/jwt.guard';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards,
+} from "@nestjs/common";
+import { EducationService } from "./education.service";
+import { JwtGuard } from "../../Auth/guard/jwt.guard";
 
 @UseGuards(JwtGuard)
-@Controller('profile/education')
+@Controller("profile/education")
 export class EducationController {
-    constructor(private education: EducationService){}
+  constructor(private education: EducationService) {}
 
-    @Post('addEducation')
-    addEducation(@Req() req ,@Body() data){
-        return this.education.addEducation(req, data);
-    }
+  @Post("addEducation")
+  addEducation(@Req() req, @Body() data) {
+    return this.education.addEducation(req, data);
+  }
+
+  @Get("getEducation")
+  getEducation(@Req() req) {
+    return this.education.getEducation(req);
+  }
+
+  @Get("getEducation/:id")
+  getEducationById(@Param("id", ParseIntPipe) id: number) {
+    return this.education.getEducationById(id);
+  }
 }
--- src/user/profile/job-sought.controller.ts
+++ src/user/profile/job-sought.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, ParseIntPipe, Req, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../../Auth/guard/jwt.guard';
 import { JobSoughtService } from './job-sought.service';
 import { JobSoughtDto } from './dto';
@@ -6,16 +6,25 @@ import { JobSoughtDto } from './dto';
 @UseGuards(JwtGuard)
 @Controller('profile/job-sought')
 export class JobSoughtController {
-    constructor(private jobService: JobSoughtService){}
+  constructor(private jobService: JobSoughtService) {}
 
-    @Post('addJobSought')
-    addJobSought(@Req() req, @Body() data: JobSoughtDto) {
-        return this.jobService.addJobSought(req, data);
-    }
+  @Post('addJobSought')
+  addJobSought(@Req() req, @Body() data: JobSoughtDto) {
+    return this.jobService.addJobSought(req, data);
+  }
 
-    @Get('GetJobSoughtUIData')
-    GetJobSoughtUIData() {
-        return this.jobService.GetJobSoughtUIData();
-    }
+  @Get('GetJobSoughtUIData')
+  GetJobSoughtUIData() {
+    return this.jobService.GetJobSoughtUIData();
+  }
 
+  @Get("GetJobSought")
+  getJobSought(@Req() req) {
+    return this.jobService.getJobSought(req);
+  }
+
+  @Get("GetJobSought/:id")
+  getJobSoughtById(@Param("id", ParseIntPipe) id: number) {
+    return this.jobService.getJobSoughtById(id);
+  }
 }
